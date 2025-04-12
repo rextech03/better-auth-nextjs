a471a5b32e3b2ef55169092211b71cfdbdbf9ca1
Aditya Singh <aditya03pratap@gmail.com>
2025-04-12 21:23:50 +0000

    Refactor authMiddleware to use getSessionCookie for faster performance

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { betterFetch } from "@better-fetch/fetch";
 import { NextResponse, type NextRequest } from "next/server";
 import type { Session } from "@/auth";
+import { getSessionCookie } from "better-auth/cookies";
 
 const authRoutes = ["/sign-in", "/sign-up"];
 const passwordRoutes = ["/reset-password", "/forgot-password"];
@@ -12,18 +13,9 @@ export default async function authMiddleware(request: NextRequest) {
   const isPasswordRoute = passwordRoutes.includes(pathName);
   const isAdminRoute = adminRoutes.includes(pathName);
 
-  const { data: session } = await betterFetch<Session>(
-    "/api/auth/get-session",
-    {
-      baseURL: process.env.BETTER_AUTH_URL,
-      headers: {
-        //get the cookie from the request
-        cookie: request.headers.get("cookie") || "",
-      },
-    },
-  );
+  const sessionCookie = getSessionCookie(request)
 
-  if (!session) {
+  if (!sessionCookie) {
     if (isAuthRoute || isPasswordRoute) {
       return NextResponse.next();
     }
@@ -34,8 +26,21 @@ export default async function authMiddleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (isAdminRoute && session.user.role !== "admin") {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (isAdminRoute) {
+    const { data: session } = await betterFetch<Session>(
+      "/api/auth/get-session",
+      {
+        baseURL: process.env.BETTER_AUTH_URL,
+        headers: {
+          //get the cookie from the request
+          cookie: request.headers.get("cookie") || "",
+        },
+      },
+    );
+
+    if (!session || session.user.role !== "admin") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 
   return NextResponse.next();
